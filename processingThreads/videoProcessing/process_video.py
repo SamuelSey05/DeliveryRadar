bb477f2945f0ca4ea4ca2a226227b890aef0223d
Samuel Seyoum <Samuel@MacBook-Pro-821.local>
2025-02-04 12:57:00 +0000

    importing modules

--- processingThreads/videoProcessing/process_video.py
+++ processingThreads/videoProcessing/process_video.py
@@ -1,13 +1,19 @@
 import os
 from common.vehicle_type import VehicleType
 import cv2
+import time
+import datetime
+import numpy as np
 
 def processVideo(id:int, vid:os.path, type:VehicleType):
     video = cv2.VideoCapture(vid)
     video.write_videofile(f"processed_videos/processed_{id}.mp4")
 
+    path = f"processed_videos/processed_{id}.mp4"
+    video = cv2.VideoCapture(path)
+
     # TODO: Process video
 
-    os.remove(f"processed_videos/processed_{id}.mp4")
+    os.remove(path)
     os.remove(vid)
     pass
