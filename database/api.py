295eff23d85678c1e6e70e8c6e0e070341ff03aa
Daniel <danielgreenhome@gmail.com>
2025-02-04 11:22:50 +0000

    Initial API plan

---
+++ database/api.py
@@ -0,0 +1,11 @@
+## TODO
+
+def insert_data():
+    pass
+
+def get_incidents():
+    """
+    Returns a list of (location (3-Tuple of String), vehicle type (Enum), time (DateTime), speed (Float)) tuples
+    """     
+    return []  
+    
