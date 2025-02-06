717954d11f413e98f6c34e400b883cccfdfbf202
jessicapeck <jess@pecksplace.com>
2025-02-06 12:42:15 +0000

    Initial object detection

--- processingThreads/videoProcessing/process_video.py
+++ processingThreads/videoProcessing/process_video.py
@@ -6,10 +6,36 @@ def processVideo(id:int, vid:os.path, type:VehicleType):
     video = cv2.VideoCapture(vid)
     video.write_videofile(f"processed_videos/processed_{id}.mp4")
 
-    # TODO: Process video
-
     path = f"processed_videos/processed_{id}.mp4"
-    video = cv2.VideoCapture(path)
+    capture = cv2.VideoCapture(path)
+
+    # define object detection model
+    bike_cascade = cv2.CascadeClassifier("models/bike.xml")
+
+    # initialise frame
+    frame_data = []
+    frame_number = 1
+
+    # TODO : change from while True to something more fail-safe
+    while True:
+        ret, frame = capture.read()
+
+        if (ret):
+            # detect bikes
+            grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            bikes = bike_cascade.detectMultiScale(grey, 1.1, 0)
+
+            for (x, y, w, h) in bikes:
+                # cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
+                frame_data.append((frame_number, int(x),int(y),int(w),int(h)))
+
+            frame_number += 1
+            # cv2.imshow('frame', frame)
+
+        else:
+            break
+    
+    capture.release()
 
 
     os.remove(f"processed_videos/processed_{id}.mp4")
