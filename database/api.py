1480663b712e5af481c29179a67e845af1531275
Daniel <danielgreenhome@gmail.com>
2025-02-10 17:16:31 +0000

    Setup Database Fixes #22

--- database/api.py
+++ database/api.py
@@ -1,9 +1,13 @@
 ## TODO
 
-def insert_data():
+from controller import DBController
+
+_controller = DBController()
+
+def insertData():
     pass
 
-def get_incidents():
+def getIncidents():
     """
     Returns a list of (location (3-Tuple of String), vehicle type (Enum), time (DateTime), speed (Float)) tuples
     """     
