5d6ea3e900344d7763b085e40f0472682e585309
Daniel <dg683@srcf.net>
2025-02-11 12:17:07 +0000

    Fixed connection problems to DB

--- database/controller.py
+++ database/controller.py
@@ -1,7 +1,11 @@
 import mysql.connector as connector
 from os import getenv as env
+import os.path
 from typing import List, TypedDict
 from datetime import datetime
+import sys
+import traceback
+
 
 class W3W(TypedDict):
     word1:str
@@ -23,21 +27,35 @@ class DBController:
     
     def __init__(self):
         self.connection = None
-        self.connect()
+        try:
+            self.connect()
+        except DBConnectionFailure:
+            print("Error: Failed to Connect to Database", file=sys.stderr)
+            traceback.print_exc()
+            quit()
         
     def __del__(self):
         self.disconnect()
             
     def connect(self) -> connector.MySQLConnection:
         if self.connection == None:
-            self.connection = connector.connect(host="mysql.internal.srcf.net", user="deliveryradar", password=env("DELIVERYRADAR_DB_PWD"), database="cstdeliveryradar")
+            try:
+                if os.path.isfile(os.path.abspath("db_pwd")):
+                    with open(os.path.abspath("db_pwd"), "r") as f:
+                        self.connection = connector.connect(host="mysql.internal.srcf.net", user="cstdeliveryradar", password=f.read(), database="cstdeliveryradar")
+                else:
+                    self.connection = connector.connect(host="mysql.internal.srcf.net", user="cstdeliveryradar", password=env("DELIVERYRADAR_DB_PWD"), database="cstdeliveryradar")
+            except:
+                
+                raise DBConnectionFailure
         return self.connection
     
     def disconnect(self) -> None:
-        if not self.connection.is_connected():
-            raise DBConnectionFailure
-        else:
-            self.connection.disconnect()
+        if self.connection != None:
+            if not self.connection.is_connected():
+                raise DBConnectionFailure
+            else:
+                self.connection.disconnect()
         
     # def _sqlCommand(self, command:str) -> List[connector.RowType]:
     #     if not self.connection.is_connected():
@@ -60,7 +78,9 @@ class DBController:
                 row = cursor.fetchone()
                 print(row)
                 tmp = DBRow(id = row[0], speed=row[1], time=[], )
+            cursor.fetchall()
             cursor.close()
+            self.connection.commit()
             return data
         
 def test():
