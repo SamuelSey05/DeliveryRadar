a77f617444a0a9bb621cb6fd3e26231bccb907d1
rkchen02 <99387528+rkchen02@users.noreply.github.com>
2025-02-11 00:53:30 +0000

    added functions to calculate speed and calculate homography

---
+++ processingThreads/object.py
@@ -0,0 +1,24 @@
+from .videoProcessing import processVideo
+
+import os.path
+
+from common import VehicleType
+
+class ProcessingThread:
+    
+    is_free:bool
+    
+    def __init__():
+        # TODO
+        pass
+            
+    def isFree(self):
+        return self.is_free
+    
+    def process(self, id:int, vid:os.path, type:VehicleType):
+        while not self.is_free:
+            pass
+        # TODO: Begin call of processVideo on new Thread
+        pass
+    
+    
