ca283475aac29205ce1283c3c0e060a8eb35907b
AxiomNine <ds2089@cam.ac.uk>
2025-02-12 16:21:35 +0000

    Built basic map component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+//import Header from "./Components/Header";
+import Map from "./Components/Map";
+
+function App() {
+  return (
+    <>
+      {/*<Header></Header>*/}
+      <Map></Map>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/HeatMap.tsx
@@ -0,0 +1,7 @@
+import Map from "./Map";
+
+function HeatMap() {
+  return <Map></Map>;
+}
+
+export default HeatMap;
