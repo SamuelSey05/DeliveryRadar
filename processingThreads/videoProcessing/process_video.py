884595c76ac57dcd1e70d06abe531b4bd4400df6
SamuelSey05 <samuel05seyoum@gmail.com>
2025-02-13 11:24:31 +0000

    refactored for multiple bikes

--- processingThreads/videoProcessing/process_video.py
+++ processingThreads/videoProcessing/process_video.py
@@ -1,5 +1,5 @@
 import os
-from common.vehicle_type import VehicleType
+# from common.vehicle_type import VehicleType
 import cv2
 from ultralytics import YOLO
 from typing import List, Tuple
@@ -53,24 +53,29 @@ def processVideo(id:int, vid:str):
     return (id, frames_to_speed(bike_data, fps))
 
 
-def frames_to_speed(frames: List[Tuple[int, float, float, float, float]], fps: int):
-    midpoints = np.zeros((len(frames), 2))  # 2D array for x,y coordinates
-    weights = np.zeros(len(frames))  # One less weight than frames, to match diffs
-    frame_numbers = np.zeros(len(frames))
-    prev_j = frames[0][0] - 1
-    
-    for i, (j, x, y, w, h) in enumerate(frames):
-        midpoints[i] = [x + w/2, y + h/2]
-        weights[i] = 1/(j - prev_j) # Weight is inversely proportional to the amount of time between frames
-        frame_numbers[i] = j
-        prev_j = j
+def frames_to_speed(bikes_frames: dict[int, List[Tuple[int, float, float, float, float]]], fps: int):
+    speeds = {}
 
-    diffs = np.linalg.norm(midpoints[1:] - midpoints[:-1], axis=1) # Calculate shortest differences between consecutive midpoints
-    pixels_per_second = diffs * fps * weights[1:] # Convert to pixels per second using weights to account for frames without rectangles
+    for bike_id, frames in bikes_frames.items(): # For each bike
+        midpoints = np.zeros((len(frames), 2))  # 2D array for x,y coordinates
+        weights = np.zeros(len(frames))  # One less weight than frames, to match diffs
+        frame_numbers = np.zeros(len(frames))
+        prev_j = frames[0][0] - 1
+        
+        for i, (j, x, y, w, h) in enumerate(frames):
+            midpoints[i] = [x + w/2, y + h/2]
+            weights[i] = 1/(j - prev_j) # Weight is inversely proportional to the amount of time between frames
+            frame_numbers[i] = j
+            prev_j = j
 
-    # Bin data into seconds (groups of fps frames)
-    binned_mean = binned_statistic(frame_numbers[1:], pixels_per_second, statistic='mean', bins=len(frames)//fps)
+        diffs = np.linalg.norm(midpoints[1:] - midpoints[:-1], axis=1) # Calculate shortest differences between consecutive midpoints
+        pixels_per_second = diffs * fps * weights[1:] # Convert to pixels per second using weights to account for frames without rectangles
 
-    return binned_mean.statistic # Return average speed in pixels per second for each second (group of fps frames)
+        # Bin data into seconds (groups of fps frames)
+        binned_mean = binned_statistic(frame_numbers[1:], pixels_per_second, statistic='mean', bins=len(frames)//fps)
 
-# print(processVideo(1, "processingThreads/assets/multiple_bikes/mult_bike1.mov"))
+        speeds[bike_id] = binned_mean.statistic
+
+    return speeds # Return average speed in pixels per second for each second (group of fps frames) for each bike
+
+print(processVideo(1, "processingThreads/assets/multiple_bikes/mult_bike1.mov"))
