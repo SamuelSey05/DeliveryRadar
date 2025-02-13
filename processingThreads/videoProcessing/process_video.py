09c6078efa8b2b2d03225753274585704c2e71d0
SamuelSey05 <samuel05seyoum@gmail.com>
2025-02-13 11:48:20 +0000

    optimising with np operations

--- processingThreads/videoProcessing/process_video.py
+++ processingThreads/videoProcessing/process_video.py
@@ -2,79 +2,56 @@ import os
 # from common.vehicle_type import VehicleType
 import cv2
 from ultralytics import YOLO
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import numpy as np
 from scipy.stats import binned_statistic
 from calculate_homography import compute_homography_matrix
 
-def processVideo(id:int, vid:str):
-
-    # TODO : move this somewhere else, don't what it to run every time the function is called
+def processVideo(id: int, vid: str):
     model = YOLO('yolo11n.pt')
-
-    # class ID of 'bicycle' in the model
     BIKE_ID = 1
-
-    bike_data = {}
-    frame_number = 1
+    bike_data: Dict[int, List[Tuple[int, float, float, float, float]]] = {}
+    frame_number = 0
 
     capture = cv2.VideoCapture(vid)
-    
-    # Get video properties
     fps = int(capture.get(cv2.CAP_PROP_FPS))
 
     while capture.isOpened():
         ret, frame = capture.read()
-
         if not ret:
             break
 
         results = model.track(source=frame, classes=[BIKE_ID], persist=True)
-
         boxes = results[0].boxes.xywh.tolist()
         track_ids = results[0].boxes.id.int().tolist() if results[0].boxes.id is not None else []
 
-        for box, bike_id in zip(boxes, track_ids):
+        for box, track_id in zip(boxes, track_ids):
             x, y, w, h = box
-
-            if bike_data.get(bike_id) is None:
-                bike_data[bike_id] = []
-            
-            bike_data[bike_id].append((frame_number, x, y, w, h))
+            if track_id not in bike_data:
+                bike_data[track_id] = []
+            bike_data[track_id].append((frame_number, x, y, w, h))
 
         frame_number += 1
-
-    # os.remove(vid)
-    
-    # print(bike_data)
-    # print(fps)
 
     capture.release()
 
     return (id, frames_to_speed(bike_data, fps))
 
-
-def frames_to_speed(bikes_frames: dict[int, List[Tuple[int, float, float, float, float]]], fps: int):
+def frames_to_speed(bikes_frames: Dict[int, List[Tuple[int, float, float, float, float]]], fps: int) -> Dict[int, np.ndarray]:
     speeds = {}
 
     for bike_id, frames in bikes_frames.items(): # For each bike
-        midpoints = np.zeros((len(frames), 2))  # 2D array for x,y coordinates
-        weights = np.zeros(len(frames))  # One less weight than frames, to match diffs
-        frame_numbers = np.zeros(len(frames))
-        prev_j = frames[0][0] - 1
-        
-        for i, (j, x, y, w, h) in enumerate(frames):
-            midpoints[i] = [x + w/2, y + h/2]
-            weights[i] = 1/(j - prev_j) # Weight is inversely proportional to the amount of time between frames
-            frame_numbers[i] = j
-            prev_j = j
+        if len(frames) < 2:
+            continue
+        frames_array = np.array(frames)
+        midpoints = frames_array[:, 1:3] + frames_array[:, 3:5] / 2  # 2D array for x,y coordinates
+        frame_numbers = frames_array[:, 0]
+        weights = 1 / np.diff(frame_numbers) # Calculate weights based on frame differences
 
         diffs = np.linalg.norm(midpoints[1:] - midpoints[:-1], axis=1) # Calculate shortest differences between consecutive midpoints
-        pixels_per_second = diffs * fps * weights[1:] # Convert to pixels per second using weights to account for frames without rectangles
 
         # Bin data into seconds (groups of fps frames)
-        binned_mean = binned_statistic(frame_numbers[1:], pixels_per_second, statistic='mean', bins=len(frames)//fps)
-
+        binned_mean = binned_statistic(frame_numbers[1:], diffs * fps * weights, statistic='mean', bins=len(frames) // fps)
         speeds[bike_id] = binned_mean.statistic
 
     return speeds # Return average speed in pixels per second for each second (group of fps frames) for each bike
