64092ecef528904364e1b4b771502baed500aee2
AxiomNine <ds2089@cam.ac.uk>
2025-02-16 13:58:19 +0000

    Heatmap Integrated

--- leaflet-heatmap-comp/src/Components/Map.jsx
+++ leaflet-heatmap-comp/src/Components/Map.jsx
@@ -1,6 +1,8 @@
 import "leaflet/dist/leaflet.css";
 import { LayersControl, MapContainer, TileLayer} from "react-leaflet";
-//import Heatmap from "./Heatmap";
+import HeatmapLayerFactory from "@vgrid/react-leaflet-heatmap-layer/cjs/HeatmapLayer";
+
+const HeatmapLayer = HeatmapLayerFactory();
 
 const mapContainerStyle = {
   width: "100vw",
@@ -14,7 +16,13 @@ const mapContainerStyle = {
 
 const centre = [52.211, 0.092];
 
+const points = []
+
 export default function Map() {
+  let i;
+  for (i = 0; i < 10; i++) {
+    points.push([centre[0], centre[1] + 0.0001 * i, 0.0001]);
+  }
   return <div className="map-container"><MapContainer center={ centre } zoom={20} style = { mapContainerStyle } scrollWheelZoom = {true}>
     <LayersControl>
       <LayersControl.BaseLayer name="Base" checked>
@@ -23,7 +31,10 @@ export default function Map() {
       url= "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png">
     </TileLayer>
     </LayersControl.BaseLayer>
-    <LayersControl.Overlay>
+    <LayersControl.Overlay name="Heatmap" checked>
+      <HeatmapLayer fitBoundsOnLoad fitBoundsOnUpdate points={points} longitudeExtractor={m => m[1]} latitudeExtractor={m => m[0]} intensityExtractor={m => parseFloat(m[2])}>
+
+      </HeatmapLayer>
       
       
     </LayersControl.Overlay>
