742d96291c786ae83611f46c87d1f0f1a2f71f81
Daniel <danielgreenhome@gmail.com>
2025-02-17 14:23:22 +0000

    Update Video Queue API

--- videoQueue/api.py
+++ videoQueue/api.py
@@ -1,11 +1,15 @@
 import os
 
-def upload(loc:os.path) -> None:
+def upload(loc:os.path) -> str:
+    
     """
     Uploads a new video into the queue
 
     Args:
         loc (os.path): the location of the .zip file containing the video and `incident.json`
+        
+    Returns:
+        str: sha256 hash of the submission
     """    
     pass
 
