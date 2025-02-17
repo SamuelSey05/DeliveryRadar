500f81a1ee932ec65d0708ca811a36f9ea8a30b6
Daniel <danielgreenhome@gmail.com>
2025-02-17 18:42:46 +0000

    Implement Video Queue API, fixes Implement Queue API #21

--- videoQueue/api.py
+++ videoQueue/api.py
@@ -1,17 +1,37 @@
-import os
+import os.path
+from videoQueue.controller import VideoQueue
+
+_controller = VideoQueue()
 
 def upload(loc:os.path) -> str:
-    
     """
     Uploads a new video into the queue
 
     Args:
         loc (os.path): the location of the .zip file containing the video and `incident.json`
+       
+    Raises:
+        FileNotFoundError: Submission zip not found
+        SubmissionError: Submission Failed to Pass Checks
+        CannotMoveZip: Cannot move the submission zip to the queue directory
         
     Returns:
         str: sha256 hash of the submission
     """    
-    pass
+    _controller.enqueue(loc)
 
-def dequeue() -> os.path:
-    pass
+def dequeue(target_dir:os.path) -> str:
+    """
+    Remove the front submission from the queue into target directory, returning ID of item
+
+    Args:
+        dir (os.path): Target directory to copy submission removed
+        
+    Raises:
+        CannotMoveZip: OS was unable to move the zip to the given directory
+        FileNotFoundError: Submission is missing from it's stored location, this should never occur unless some kind of cleanup happens on the tempdir during use
+
+    Returns:
+        str: sha256 hash of the submission removed, used as ID
+    """    
+    return _controller.dequeue(target_dir)
