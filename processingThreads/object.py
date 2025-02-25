2f1d6c62f7bbbf5ec9259cee361df3535d62974d
Daniel <danielgreenhome@gmail.com>
2025-02-25 12:28:10 +0000

    Thread Code Completed

--- processingThreads/object.py
+++ processingThreads/object.py
@@ -1,24 +1,37 @@
-from .videoProcessing import processVideo
+from processingThreads.videoProcessing import processVideo
 
-import os.path
+from multiprocessing import Pipe, Process
+from multiprocessing.connection import Connection
 
-from common import VehicleType
+from typing import Dict, List, Tuple, Union
 
-class ProcessingThread:
+from common import processingArgs, SIG_END
     
-    is_free:bool
-    
-    def __init__():
-        # TODO
-        pass
-            
-    def isFree(self):
-        return self.is_free
-    
-    def process(self, id:int, vid:os.path, type:VehicleType):
-        while not self.is_free:
-            pass
-        # TODO: Begin call of processVideo on new Thread
-        pass
-    
-    
+def thread_fun(con:Connection):
+    """
+    Function Performed by Each processing Thread
+
+    Args:
+        con (Connection): Pipe Connection End to Controller
+
+    Returns(Down Pipe):
+        Tuple[str, List[float]]: Will Send (ID, [Speeds]) back along pipe to controller
+    """
+    data:Union[processingArgs, int] = con.recv()
+    while data != SIG_END:
+        ret:tuple[str, Dict[int,float]] = processVideo(data['id'], data['path'])
+        speeds:List[float] = list(ret[1].values())
+        con.send((ret[0], speeds))
+        data = con.recv()
+
+def new_thread() -> tuple[Process, Connection]:
+    """
+    Create a new processing Thread and Connection to it
+
+    Returns:
+        tuple[Process, Connection]: Tuple of the processing Thread ID and the Pipe connection to it
+    """    
+    controller_con, thread_con = Pipe()
+    p = Process(target=thread_fun, args=[thread_con])
+    p.start()
+    return (p, controller_con)
