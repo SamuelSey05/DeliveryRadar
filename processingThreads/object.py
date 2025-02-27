2549cf9c2f1b999d7e8c4db6846dabb3ef10f81c
Daniel <danielgreenhome@gmail.com>
2025-02-27 16:49:14 +0000

    Refactor processing threads for unified return queue

--- processingThreads/object.py
+++ processingThreads/object.py
@@ -1,13 +1,13 @@
 from processingThreads.videoProcessing import processVideo
 
-from multiprocessing import Pipe, Process
+from multiprocessing import Pipe, Process, Queue
 from multiprocessing.connection import Connection
 
 from typing import Dict, List, Tuple, Union
 
 from common import processingArgs, SIG_END
     
-def thread_fun(con:Connection):
+def thread_fun(rev_con:Connection, ret_q:Queue):
     """
     Function Performed by Each processing Thread
 
@@ -17,14 +17,14 @@ def thread_fun(con:Connection):
     Returns(Down Pipe):
         Tuple[str, List[float]]: Will Send (ID, [Speeds]) back along pipe to controller
     """
-    data:Union[processingArgs, int] = con.recv()
+    data:Union[processingArgs, int] = rev_con.recv()
     while data != SIG_END:
-        ret:tuple[str, Dict[int,float]] = processVideo(data['id'], data['path'])
+        ret:tuple[int, Dict[int,float]] = processVideo(data['id'], data['path'])
         speeds:List[float] = list(ret[1].values())
-        con.send((ret[0], speeds))
-        data = con.recv()
+        ret_q.put((ret[0], speeds))
+        data = rev_con.recv()
 
-def new_thread() -> tuple[Process, Connection]:
+def new_thread(ret_q:Queue) -> tuple[Process, Connection]:
     """
     Create a new processing Thread and Connection to it
 
@@ -32,6 +32,6 @@ def new_thread() -> tuple[Process, Connection]:
         tuple[Process, Connection]: Tuple of the processing Thread ID and the Pipe connection to it
     """    
     controller_con, thread_con = Pipe()
-    p = Process(target=thread_fun, args=[thread_con])
+    p = Process(target=thread_fun, args=[thread_con, ret_q])
     p.start()
     return (p, controller_con)
