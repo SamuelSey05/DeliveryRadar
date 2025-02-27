240e79c06f6a2ff7ffd1cd50bd947d57aac22c3f
AxiomNine <ds2089@cam.ac.uk>
2025-02-27 18:13:18 +0000

    Reset all screen design values to absolute. Also, created a mobile version

--- video-upload/src/App.jsx
+++ video-upload/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import UploadModal from "./UploadModal";
 import "./App.css";
 import Map from "./Map";
@@ -6,16 +6,26 @@ import Map from "./Map";
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const [isPortrait, setIsPortrait] = useState(window.matchMedia("(orientation: portrait)").matches);
+
+  useEffect(() => {
+    const handleOrientationChange = (e) => {
+      setIsPortrait(e.matches);
+    };
+
+    window.matchMedia("(orientation: portrait)").addEventListener('change', handleOrientationChange);
+  }, []);
+
   return (
-    <div className="rowC">
-    <div className="app-container dark-theme" style={{zIndex: 1000}}>
+    <div className={isPortrait ? "columnC" : "rowC"}>
+    <div className="app-container dark-theme" style={{zIndex: 1000, left: "5%", right: (isPortrait? "5%" : "55%"), top: (isPortrait ? "55%" : "5%"), bottom: "5%"}}>
       <h1 className="app-title">Video Upload Portal</h1>
       <button className="upload-button" onClick={() => setIsModalOpen(true)} style={{height: "72px"}}>
         Upload Video
       </button>
       {isModalOpen && <UploadModal onClose={() => setIsModalOpen(false)} />}
     </div>
-    <div className="app-container dark-theme" style={{zIndex: 0}}>
+    <div className="app-container dark-theme" style={{zIndex: 1000, left: (isPortrait? "5%" : "55%"), right: "5%", top: "5%", bottom: (isPortrait ? "55%" : "5%")}}>
       <Map></Map>
     </div>
     </div>
--- video-upload/src/Map.jsx
+++ video-upload/src/Map.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import "leaflet/dist/leaflet.css";
 import { LayersControl, MapContainer, TileLayer} from "react-leaflet";
 import HeatmapLayerFactory from "@vgrid/react-leaflet-heatmap-layer/cjs/HeatmapLayer";
@@ -23,13 +24,49 @@ const centre = [52.211, 0.092];
 
 const points = []
 
-export default function Map() {
+
+function Map() {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const [vehicleType, setVehicleType] = useState("all");
+
+  useEffect(() => {
+    fetch('cstdeliveryradar.soc.srcf.net/heatmap-data')
+      .then(response => {
+        if(!response.ok) {
+          throw new Error("Heatmap data is unavailable for some reason.");
+        }
+        return response.json();
+      })
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      })
+
+  }, []);
+
+
   let i;
   for (i = 0; i < 10; i++) {
-    points.push([centre[0], centre[1] + 0.0001 * i, 0.0001]);
+    points.push([{
+      long: centre[0], 
+      lat: centre[1] + 0.0001 * i,
+      intensity: 0.0001}]);
   }
+
+  if (loading) return <div>Data is loading, wait a sec...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+
   return <div className="map-container"><MapContainer center={ centre } zoom={20} style = { mapContainerStyle } scrollWheelZoom = {true}>
-    <LayersControl style = {layersControlStyle}>
+    <LayersControl style = {layersControlStyle}> {/* */}
       <LayersControl.BaseLayer name="Base" checked style = {{layersControlStyle}}>
     <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -37,7 +74,7 @@ export default function Map() {
     </TileLayer>
     </LayersControl.BaseLayer>
     <LayersControl.Overlay name="Heatmap" checked style = {{layersControlStyle}}>
-      <HeatmapLayer fitBoundsOnLoad fitBoundsOnUpdate points={points} longitudeExtractor={m => m[1]} latitudeExtractor={m => m[0]} intensityExtractor={m => parseFloat(m[2])} style = {{layersControlStyle}}>
+      <HeatmapLayer fitBoundsOnLoad fitBoundsOnUpdate points={{/*JSON.parse(data)*/points}} longitudeExtractor={m => m.long} latitudeExtractor={m => m.lat} intensityExtractor={m => parseFloat(m.intensity)} style = {{layersControlStyle}}>
 
       </HeatmapLayer>
       
@@ -47,3 +84,5 @@ export default function Map() {
   </MapContainer>
   </div>
 }
+
+export default Map;
