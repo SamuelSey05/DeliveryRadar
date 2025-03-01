fe591d30833f7961d3dad43b0ea9fa739d4895d7
Daniel <danielgreenhome@gmail.com>
2025-03-01 17:09:24 +0000

    Rewrite Scheduler-Thread interface to use Queues instead of Pipes

--- processingThreads/thread.py
+++ processingThreads/thread.py
@@ -1,7 +1,6 @@
 from processingThreads.videoProcessing import processVideo
 
-from multiprocessing import Pipe, Process, Queue
-from multiprocessing.connection import Connection
+from multiprocessing import Process, Queue
 
 from typing import Dict, List, Tuple, Union
 
@@ -9,28 +8,36 @@ from common import processingArgs, SIG_END
 
 import os.path
     
-def thread_fun(con:Connection, ret_q:Queue):
+def thread_fun(ctrl_q:Queue, ret_q:Queue, ret_q_shared:Queue):
     """
     Function Performed by Each processing Thread
 
     Args:
-        con (Connection): Pipe Connection End to Controller
+        ctrl_q (Queue): Connection End to Controller
+        ret_q (Queue): Connection to Return values down
+        ret_q_shared (Queue): Queue to return ID down
+    """    
+    """
+    
+
+    Args:
+        con (Connection): 
 
     Returns(Down Pipe):
         Tuple[str, List[float]]: Will Send (ID, [Speeds]) back along pipe to controller
     """
-    data:Union[processingArgs, int] = con.recv()
+    data:Union[processingArgs, int] = ctrl_q.get()
     while data != SIG_END:
         vid_id:str = data['vid_id']
         thr_id:int = data['thr_id']
         path:os.path = data['path']
         ret:tuple[str, Dict[int,float]] = processVideo(vid_id, path)
         speeds:List[float] = list(ret[1].values())
-        con.send((vid_id, speeds))
-        ret_q.put(thr_id)
-        data = con.recv()
+        ret_q.put((vid_id, speeds))
+        ret_q_shared.put(thr_id)
+        data = ctrl_q.get()
 
-def new_thread(ret_q:Queue) -> tuple[Process, Connection]:   
+def new_thread(ret_q_shared:Queue) -> tuple[Process, Queue, Queue]:   
     """
     Create a new processing Thread and Connection to it
 
@@ -40,7 +47,8 @@ def new_thread(ret_q:Queue) -> tuple[Process, Connection]:
     Returns:
         tuple[Process, Connection]: Tuple of the processing Thread ID and the Pipe connection to it
     """    
-    controller_con, thread_con = Pipe()
-    p = Process(target=thread_fun, args=[thread_con, ret_q])
+    ctrl_q = Queue()
+    ret_q = Queue()
+    p = Process(target=thread_fun, args=[ctrl_q, ret_q, ret_q_shared])
     p.start()
-    return (p, controller_con)
+    return (p, ctrl_q, ret_q)
